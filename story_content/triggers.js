debc0980763172be39d4eeeb278fe71329a2f86b
Natalia <105737285+natalia-bir@users.noreply.github.com>
2025-07-17 18:13:31 +0200

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,215 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5Vrc4FdmDLc":
+        Script1();
+        break;
+      case "5mWsn3v71Ct":
+        Script2();
+        break;
+      case "6RhcCA8vOoT":
+        Script3();
+        break;
+      case "5jjoocrfzBx":
+        Script4();
+        break;
+      case "6VGJegZ75vg":
+        Script5();
+        break;
+      case "6nWXCD5RX4c":
+        Script6();
+        break;
+      case "6gMs1GxYJCg":
+        Script7();
+        break;
+      case "65dQJUfgKrI":
+        Script8();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('5ba1BQNWADc');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5beEnnobTvg';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6YOkLRBnzue');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6OYZrjuL3AI';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('5bBQxLCPyio');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6c8zEWHelHv';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('5WdhtHecsLC');
+const duration = 750;
+const easing = 'ease-out';
+const id = '69bUlplIMJz';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('5dtLc9d7ufK');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6nFzhGVjm2B';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('6YOkLRBnzue');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6OYZrjuL3AI';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script7 = function()
+{
+  const target = object('5bBQxLCPyio');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6c8zEWHelHv';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script8 = function()
+{
+  const target = object('5dtLc9d7ufK');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6nFzhGVjm2B';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
